2668ebe1faf1cc8551e5a834cf3f51497a12c2f6
Shiv290899 <Knshivakumar139@gmail.com>
2025-06-26 00:27:15 +0530

    Fix: Handle missing or malformed authorization header in authMiddleware

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -6,8 +6,12 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).send({ success: false, message: "No token provided" });
+    }
 
+    const token = authHeader.split(" ")[1];
     const verifiedToken = jwt.verify(token, "scaler_movies");
 
     req.body.userId = verifiedToken.userId;
